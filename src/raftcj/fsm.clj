1737c95cc36562790df1f61b03c43d93c2a7d91a
Francesco Degrassi <fdegrassi@gmail.com>
2014-09-10 18:40:30 +0100

    ref: cleaner code

--- src/raftcj/fsm.clj
+++ src/raftcj/fsm.clj
@@ -5,7 +5,13 @@
   (defn msg [target type & args]
     (concat [target type] args))
 
-  (declare executed)
+  (defn redispatch [[state, msgs] event & args]
+    (let [
+      [state, moremsgs] (apply (partial event state) args)]
+      [state (concat msgs moremsgs)]))
+
+  (defn executed [client success] 
+    :todo)
 
   (defn become-follower [state term]
     (let [
@@ -41,13 +47,8 @@
 
   (declare elected advertise-leader)
 
-  (defn redispatch [[state, msgs] event & args]
-    (let [
-      [state, moremsgs] (apply (partial event state) args)]
-      [state (concat msgs moremsgs)]))
   
-  ; TODO: macro to redispatch on newer term?
-
+  
   (defmacro defev [evname selector rest failmsgs body] 
     `(defmethod ~evname ~selector ~(vec (concat ['state 'term] rest))
       (cond 
@@ -74,8 +75,6 @@
           (elected (become-leader state))
           [state []]))))
   
-  ; TODO: macro to reply false on old term?
-
   (defmulti request-vote state-of)
   (defev request-vote :default [candidate-id last-log-index last-log-term]
     [(msg candidate-id voted (:current-term state) (:id state) false)]
@@ -170,15 +169,22 @@
       is-from-current-term (fn [idx] (= current-term (:term (log idx))))]
       (first (filter is-from-current-term uncommitted-replicated-indexes))))
 
-  (declare update-msg)
-  (declare executed)
+  (declare append-entries)
+  (defn update-msg [state peer idx]
+    (let [
+      prev-log-index (dec idx)
+      prev-log-entry (get (:log state) prev-log-index)
+      entries (subvec (:log state) idx)]
+      (msg peer append-entries (:current-term state) (:id state) prev-log-index (:term prev-log-entry) entries (:commit-index state))))
 
   (defmulti appended state-of)
   (defev appended :default [appender next-index success]
     []
     (if success
       (let [
-          state (assoc-in (assoc-in state [:next-index appender] next-index) [:next-match appender] next-index)
+          state (-> state 
+            (assoc-in [:next-index appender] next-index)
+            (assoc-in [:next-match appender] next-index))
           old-commit-index (:commit-index state)
           commit-index (if-let 
             [updated (new-commit-index (get-in state [:config :members]) (:current-term state) (:log state) (vals (:next-match state)) old-commit-index)]
@@ -212,13 +218,6 @@
         append-entries
         term leader-id prev-log-index prev-log-term entries leader-commit))
 
-  (defn update-msg [state peer idx]
-    (let [
-      prev-log-index (dec idx)
-      prev-log-entry (get (:log state) prev-log-index)
-      entries (subvec (:log state) idx)]
-      (msg peer append-entries (:current-term state) (:id state) prev-log-index (:term prev-log-entry) entries (:commit-index state))))
-
   (defn advertise-leader [state] ;TODO: test
     (let [
       heartbeats (vec (map #(apply (partial update-msg state) %)(:next-index state)))
@@ -227,9 +226,6 @@
 
   (defn elected [state]
     [state (advertise-leader state)])
-
-  (defn executed [client success] 
-    :todo)
 
   (defmulti execute state-of)
   (defmethod execute :leader [state client cmd]
@@ -240,6 +236,6 @@
       needing-update (filter (fn [peer idx] (>= last-log-index idx)) (:next-index state))
       updates (vec (map #(apply (partial update-msg state) %) needing-update))] ; TODO: test
       [state updates]))
+
 ; TODO: RPC, not messages
 ; TODO: create part-of-cluster check logic outside ?
-
