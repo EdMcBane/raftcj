1250666c41287faf6ac112ef704855ad32634b58
Francesco Degrassi <fdegrassi@gmail.com>
2014-09-13 13:06:24 +0100

    dev: conf for clojurescript

--- project.clj
+++ project.clj
@@ -2,8 +2,19 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+    [org.clojure/clojurescript "0.0-2311"]]
+  :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
+  :source-paths ["src"]
   :main ^:skip-aot raftcj.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :cljsbuild {
+    :builds [{:id "raftcj"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "raftcj.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
