5de7255eeb73e2a120cb279ea20297462772d686
Francesco Degrassi <fdegrassi@gmail.com>
2014-09-16 12:30:40 +0100

    dev: rudimentary html UI

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
   :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-    [org.clojure/clojurescript "0.0-2311"]]
+    [org.clojure/clojurescript "0.0-2311"]
+    [prismatic/dommy "0.1.3"]]
   :plugins [
     [lein-cljsbuild "1.0.4-SNAPSHOT"]
     [lein-simpleton "1.3.0"]
--- src/raftcj/core.cljs
+++ src/raftcj/core.cljs
@@ -2,7 +2,13 @@
 	(:require 
         cljs.reader
 		[raftcj.loop :refer [eventloop]]
-		[raftcj.base :refer [initial-state msg bad-arg]]))
+		[raftcj.base :refer [initial-state msg bad-arg]]
+        
+        [dommy.utils :as utils]
+        [dommy.core :as dommy])
+    (:use-macros
+        [dommy.macros :only [node sel sel1]])
+    )
 
 (enable-console-print!)
 
@@ -106,3 +112,31 @@
             ;   45 45
             }}]
         (webrtc config id "plh3877i44fmkj4i")))
+
+
+(defn display-state []
+    (dommy/set-text! (sel1 :#id) (:id @world))
+    (dommy/set-text! (sel1 :#state) (:statename @world))
+    (dommy/set-text! (sel1 :#log) (:log @world))
+    (dommy/set-text! (sel1 :#commit) (:commit-index @world))
+    (dommy/set-text! (sel1 :#fsm) (:fsm @world)))
+
+(defn start-polling []
+    (js/setTimeout (fn [] (start-polling) (display-state)) 100))
+
+(defn start-button [id]
+    (let [
+        btn (node [:button#start-btn {} (str "Start " id)])]
+        (dommy/listen! btn :click 
+            #(do (-main id) (start-polling)))
+        btn))
+
+(defn setup-ui []
+    (apply (partial dommy/append! (sel1 :#cluster))
+        (map start-button [0 12 23]))
+
+    (dommy/listen! 
+        (sel1 :#execute-btn) :click 
+        #(let [
+            cmd (dommy/value (sel1 :#cmd-field))]
+            (exec cmd))))
